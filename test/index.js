9436e8dbe39475c7eaed10c0fe7b64f25f99cf42
Titus Wormer <tituswormer@gmail.com>
2023-04-03 10:17:38 +0200

    Use Node test runner

--- test/index.js
+++ test/index.js
@@ -1,24 +1,24 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import test from 'node:test'
 import {micromark} from 'micromark'
 import {gfmTagfilterHtml as html} from '../index.js'
 
-const input = fs.readFileSync(path.join('test', 'input.md'))
-const output = fs.readFileSync(path.join('test', 'output.html'), 'utf8')
+const input = await fs.readFile(new URL('input.md', import.meta.url))
+const output = String(
+  await fs.readFile(new URL('output.html', import.meta.url))
+)
 
-test('markdown -> html (micromark)', (t) => {
-  t.deepEqual(
+test('markdown -> html (micromark)', () => {
+  assert.equal(
     micromark(input, {allowDangerousHtml: true, htmlExtensions: [html]}),
     output,
     'should support a tag filter just like how GH does it'
   )
 
-  t.deepEqual(
+  assert.equal(
     micromark('a <i>\n<script>', {htmlExtensions: [html]}),
     '<p>a &lt;i&gt;</p>\n&lt;script&gt;',
     'should not turn `allowDangerousHtml` on'
   )
-
-  t.end()
 })
